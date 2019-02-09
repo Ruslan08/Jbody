6ee3a3dccd91a7c467ead95c13cec9183948e138
Ruslan08 <dibirov1993@gmail.com>
2019-02-10 00:08:55 +0300

    add test for CsvFile

---
+++ src/test/java/CsvFileTest.java
@@ -0,0 +1,32 @@
+import jbody.CsvFile;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class CsvFileTest {
+
+    private URI file;
+
+    @Before
+    public void init() throws URISyntaxException {
+        this.file = ClassLoader.getSystemResource("test.csv").toURI();
+    }
+
+    @Test
+    public void contentTest() {
+        double[][] data = new CsvFile(file).data();
+
+        Assert.assertArrayEquals(
+                new double[]{1.2390996043280014,0.9496296686258394,0.4508758419116252,4.610462531531555E-8,2.1314583151345376E-7,-2.648710970351495E-7,1},
+                data[0],
+                0);
+        Assert.assertArrayEquals(
+                new double[]{-0.18783804137816418,0.2832099512289661,0.1970794551787467,-2.454145375143699E-7,-2.975835624479583E-7,-5.306079785129336E-7,1},
+                data[1],
+                0);
+    }
+
+}
