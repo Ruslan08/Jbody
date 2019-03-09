c51dd41cde2f9e95e8f0573e39fcf512e08ebf45
Ruslan08 <eto_ya08@mail.ru>
2019-03-10 01:31:21 +0300

    data snapshot

---
+++ src/main/java/jbody/DirectoryNextToFile.java
@@ -0,0 +1,36 @@
+package jbody;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Paths;
+
+/**
+ * Represent the directory next to the source file
+ */
+public class DirectoryNextToFile {
+
+    private final URI sourcePath;
+    private final String directoryName;
+
+    public DirectoryNextToFile(URI path, String dirName) {
+        this.sourcePath = path;
+        this.directoryName = dirName;
+    }
+
+    public DirectoryNextToFile(String path, String dirName) {
+        try {
+            this.sourcePath = new URI(path);
+            this.directoryName = dirName;
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public URI pathToDirectory() {
+        return Paths.get(sourcePath)
+                .getParent()
+                .resolve(directoryName)
+                .toUri();
+    }
+
+}
